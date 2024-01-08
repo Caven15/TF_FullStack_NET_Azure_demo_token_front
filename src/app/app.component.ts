c0cb74ebac9e97e3d1d6e8d90504696be3cd4b83
Caven15 <cavenditch15@gmail.com>
2024-01-08 10:20:15 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'TF_FullStack_NET_Azure_demo_token_front';
+}
